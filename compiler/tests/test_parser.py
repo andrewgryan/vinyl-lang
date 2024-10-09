0d2cc225a6aca3f722fa37c9810a043d886e934d
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-10-09 20:11:26 +0100

    add failing test

--- compiler/tests/test_parser.py
+++ compiler/tests/test_parser.py
@@ -1,5 +1,5 @@
 import pytest
-from compiler.main import parse, let, exit
+from compiler.main import parse, let, exit, NodeExit, Token
 
 
 @pytest.mark.parametrize("content,statements", [
@@ -7,7 +7,8 @@ from compiler.main import parse, let, exit
     ("# Comment", []),
     (";", []),
     ("let foo = 42;", [let("foo", "42")]),
-    ("exit(42);", [exit("42")])
+    ("exit(42);", [exit("42")]),
+    ("exit(foo);", [NodeExit(status=Token.identifier("foo"))])
 ])
 def test_parse(content, statements):
     assert parse(content).statements == statements
