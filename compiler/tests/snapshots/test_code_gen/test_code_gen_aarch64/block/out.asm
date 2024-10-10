42136e8993bf0b0067c7183acd43f9f7a7d3b1bd
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-10-10 22:55:14 +0100

    missing snapshot

---
+++ compiler/tests/snapshots/test_code_gen/test_code_gen_aarch64/block/out.asm
@@ -0,0 +1,15 @@
+.global _start
+.section .text
+
+_start:
+        sub sp, sp, #0x10
+        mov w0, #0x1
+        str w0, [sp, #0x8]
+        sub sp, sp, #0x8
+        mov w0, #0x2a
+        str w0, [sp, #0x8]
+        add sp, sp, #0x8
+        mov x8, #0x5d
+        ldr x0, [sp, #0x8]
+        svc 0
+        add sp, sp, #0x10
