7f3f50112e2187436d1a8cf2f97994b2efde8ee3
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-10-28 22:39:02 +0000

    use quadruple IR

--- compiler/src/compiler/ir.py
+++ compiler/src/compiler/ir.py
@@ -1,33 +1,130 @@
-from dataclasses import dataclass
-
-COPY = "COPY"
-RETF = "RETF"
-
-
-@dataclass
-class Lit:
-    value: int
+"""
+Intermediate representation
+"""
+from compiler import parser
 
 
-@dataclass
-class Var:
-    name: str
-    type: str
-    owner: str
+def visit_int(node):
+    return int(node.token.text)
 
 
-def translate(ast):
-    ops = []
-    for statement in ast.statements:
-        if hasattr(statement, "identifier"):
-            id = statement.identifier.token.text
-            value = int(statement.value.token.text)
-            op = (
-                COPY,
-                Lit(value),
-                Var(id, "u32", "_start"),
-            )
+def visit_expression(node):
+    if is_int(node):
+        return visit_int(node)
+    elif is_binop(node):
+        return visit_binop(node)
+    elif is_identifier(node):
+        return visit_identifier(node)
+    else:
+        raise Exception(node)
+
+
+def visit_binop(binop):
+    lhs = visit_expression(binop.lhs)
+    rhs = visit_expression(binop.rhs)
+    if binop.operator.operator == "+":
+        try:
+            return lhs + rhs
+        except TypeError:
+            return ("ADD", lhs, rhs)
+    elif binop.operator.operator == "*":
+        return lhs * rhs
+
+
+def visit_statement(node):
+    if is_exit(node):
+        return visit_exit(node)
+    elif is_let(node):
+        return visit_let(node)
+    elif is_print(node):
+        return visit_print(node)
+    elif is_function(node):
+        return visit_function(node)
+    elif is_block(node):
+        return visit_block(node)
+    else:
+        raise Exception(node)
+
+
+def visit_exit(node):
+    return [
+        ("exit", visit_expression(node.status), None, None)
+    ]
+    # return ("EXIT", visit_expression(node.status))
+
+
+def visit_print(node):
+    return visit_expression(node.message)
+
+
+def visit_let(node):
+    expr = visit_expression(node.value)
+    return (
+        "=",
+        visit_identifier(node.identifier),
+        visit_expression(node.value),
+        None
+    )
+
+
+def visit_function(node):
+    pre = [("FUNCTION", "START")]
+    post = [("FUNCTION", "END")]
+    return pre + visit_statements(node.body.statements) + post
+
+
+def visit_block(node):
+    pre = [("BLOCK", "START")]
+    post = [("BLOCK", "END")]
+    return pre + visit_statements(node.statements) + post
+
+
+def visit_identifier(node):
+    return node.token.text
+
+
+def visit_statements(statements):
+    numbers = []
+    for statement in statements:
+        number = visit_statement(statement)
+        if isinstance(number, list):
+            numbers += number
         else:
-            op = (RETF, Lit(5))
-        ops.append(op)
-    return ops
+            numbers.append(number)
+    return numbers
+
+
+def visit(ast):
+    return visit_statements(ast.statements)
+
+
+def is_exit(node):
+    return isinstance(node, parser.NodeExit)
+
+
+def is_print(node):
+    return isinstance(node, parser.NodePrint)
+
+
+def is_binop(node):
+    return isinstance(node, parser.NodeBinOp)
+
+
+def is_int(node):
+    return isinstance(node, parser.NodeInt)
+
+
+def is_let(node):
+    return isinstance(node, parser.NodeLet)
+
+
+def is_function(node):
+    return isinstance(node, parser.NodeFunction)
+
+
+def is_identifier(node):
+    return isinstance(node, parser.NodeIdentifier)
+
+
+def is_block(node):
+    return isinstance(node, parser.NodeBlock)
--- compiler/tests/test_ir.py
+++ compiler/tests/test_ir.py
@@ -2,204 +2,37 @@ from compiler import ir, parser
 import pytest
 
 
-@pytest.mark.parametrize(
-    "code,expected",
-    [
-        ("return 5;", [(ir.RETF, ir.Lit(5))]),
-        (
-            "let x = 1;let y = 2;",
-            [
-                (
-                    ir.COPY,
-                    ir.Lit(1),
-                    ir.Var("x", "u32", "_start"),
-                ),
-                (
-                    ir.COPY,
-                    ir.Lit(2),
-                    ir.Var("y", "u32", "_start"),
-                ),
-            ],
-        ),
-    ],
-)
-def test_ir(code, expected):
-    ast = parser.parse(code)
-    actual = ir.translate(ast)
-    assert actual == expected
-
-
-def visit_int(node):
-    return int(node.token.text)
-
-
-def visit_expression(node):
-    if is_int(node):
-        return visit_int(node)
-    elif is_binop(node):
-        return visit_binop(node)
-    elif is_identifier(node):
-        return visit_identifier(node)
-    else:
-        raise Exception(node)
-
-
-def visit_binop(binop):
-    lhs = visit_expression(binop.lhs)
-    rhs = visit_expression(binop.rhs)
-    if binop.operator.operator == "+":
-        try:
-            return lhs + rhs
-        except TypeError:
-            return ("ADD", lhs, rhs)
-    elif binop.operator.operator == "*":
-        return lhs * rhs
-
-
-def visit_statement(node):
-    if is_exit(node):
-        return visit_exit(node)
-    elif is_let(node):
-        return visit_let(node)
-    elif is_print(node):
-        return visit_print(node)
-    elif is_function(node):
-        return visit_function(node)
-    elif is_block(node):
-        return visit_block(node)
-    else:
-        raise Exception(node)
-
-
-def visit_exit(node):
-    return ("EXIT", visit_expression(node.status))
-
-
-def visit_print(node):
-    return visit_expression(node.message)
-
-
-def visit_let(node):
-    expr = visit_expression(node.value)
-    return (
-        "LET",
-        visit_identifier(node.identifier),
-        visit_expression(node.value),
-    )
-
-
-def visit_function(node):
-    pre = [("FUNCTION", "START")]
-    post = [("FUNCTION", "END")]
-    return pre + visit_statements(node.body.statements) + post
-
-
-def visit_block(node):
-    pre = [("BLOCK", "START")]
-    post = [("BLOCK", "END")]
-    return pre + visit_statements(node.statements) + post
-
-
-def visit_identifier(node):
-    return node.token.text
-
-
-def visit_statements(statements):
-    numbers = []
-    for statement in statements:
-        number = visit_statement(statement)
-        if isinstance(number, list):
-            numbers += number
-        else:
-            numbers.append(number)
-    return numbers
-
-
-def visit(ast):
-    return visit_statements(ast.statements)
-
-
-def is_exit(node):
-    return isinstance(node, parser.NodeExit)
-
-
-def is_print(node):
-    return isinstance(node, parser.NodePrint)
-
-
-def is_binop(node):
-    return isinstance(node, parser.NodeBinOp)
-
-
-def is_int(node):
-    return isinstance(node, parser.NodeInt)
-
-
-def is_let(node):
-    return isinstance(node, parser.NodeLet)
-
-
-def is_function(node):
-    return isinstance(node, parser.NodeFunction)
-
-
-def is_identifier(node):
-    return isinstance(node, parser.NodeIdentifier)
-
-
-def is_block(node):
-    return isinstance(node, parser.NodeBlock)
-
-
 def test_visitor():
     ast = parser.parse(
         """
-        let x = 1 + 1 + 2 + 3;
-        fn example() {
-            let code = x + 16;
-            {
-                let scoped = 3;
-            }
-            exit(code);
-        }
-
-        let t = 10 * 10;
+        let x = 3;
+        exit(0);
     """
     )
-    assert is_let(ast.statements[1].body.statements[0])
-    assert visit(ast) == [
-        ("LET", "x", 7),
-        ("FUNCTION", "START"),
-        ("LET", "code", ("ADD", "x", 16)),
-        ("BLOCK", "START"),
-        ("LET", "scoped", 3),
-        ("BLOCK", "END"),
-        ("EXIT", "code"),
-        ("FUNCTION", "END"),
-        ("LET", "t", 100),
+    assert ir.visit(ast) == [
+        ("=", "x", 3, None),
+        ("exit", 0, None, None)
     ]
 
 
 def gas(instructions):
-    registers = {"r1": "rax"}
-    for inst in instructions:
-        if inst[0] == "LABEL":
-            yield f"{inst[1]}:"
-        elif inst[0] == "MOV":
-            src = f"${inst[1]}"
-            dst = f"%{registers[inst[2]]}"
-            yield f"\tmov {src}, {dst}"
+    for (op, arg1, arg2, result) in instructions:
+        if op == "=":
+            yield f"mov ${arg2}, -0x8(%rbp)"
+        elif op == "exit":
+            yield f"mov $60, %rax"
+            yield f"mov ${arg1}, %rdi"
+            yield f"syscall"
 
 
 def aarch64(instructions):
-    registers = {"r1": "x1"}
-    for inst in instructions:
-        if inst[0] == "LABEL":
-            yield f"{inst[1]}:"
-        elif inst[0] == "MOV":
-            src = f"#{hex(inst[1])}"
-            dst = f"{registers[inst[2]]}"
-            yield f"\tmov {dst}, {src}"
+    for (op, arg1, arg2, result) in instructions:
+        if op == "=":
+            yield f"mov [sp, #0x8], #{hex(arg2)}"
+        elif op == "exit":
+            yield f"mov x8, #0x5d"
+            yield f"mov x0, #{hex(arg1)}"
+            yield f"svc 0"
 
 
 def render(lines):
@@ -207,6 +40,16 @@ def render(lines):
 
 
 def test_ir_code_gen():
-    ir = [("LABEL", "L1"), ("MOV", 5, "r1")]
-    assert list(gas(ir)) == ["L1:", "\tmov $5, %rax"]
-    assert list(aarch64(ir)) == ["L1:", "\tmov x1, #0x5"]
+    ir = [("=", "x", 3, None), ("exit", 0, None, None)]
+    assert list(gas(ir)) == [
+        "mov $3, -0x8(%rbp)",
+        "mov $60, %rax",
+        "mov $0, %rdi",
+        "syscall",
+    ]
+    assert list(aarch64(ir)) == [
+        "mov [sp, #0x8], #0x3",
+        "mov x8, #0x5d",
+        "mov x0, #0x0",
+        "svc 0",
+    ]
